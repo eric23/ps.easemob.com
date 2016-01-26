731b28333800d2e0dfecd37278009fc1eee4e334
eric23 <yanjun@easemob.com>
2016-01-26 20:42:55 +0800

    update on 0126: translation and hide additional menu

--- src/l10n/en.js
+++ src/l10n/en.js
@@ -1,93 +1,121 @@
 {
-  "header": {
-    "navbar": {
-      "UPLOAD": "Upload",
-      "new": {
-        "NEW": "New",
-        "PROJECT": "Projects",
-        "TASK": "Task",
-        "USER": "User",
-        "EMAIL": "Email"
-      },
-      "NOTIFICATIONS": "Notifications"
-    }
-  },
-  "aside": {
-    "nav": {
-      "ps": {
-        "HEADER": "Navigation",
-        "PRICE": "Price List"
-      },
-      "HEADER": "Navigation",
-      "DASHBOARD": "Dashboard",
-      "CALENDAR": "Calendar",
-      "EMAIL": "Email",
-      "WIDGETS": "Widgets",
-      "components": {
-        "COMPONENTS": "Components",
-        "ui_kits": {
-          "UI_KITS": "UI Kits",
-          "BUTTONS": "Buttons",
-          "ICONS": "Icons",
-          "GRID": "Grid",
-          "BOOTSTRAP": "Bootstrap",
-          "SORTABLE": "Sortable",
-          "PORTLET": "Portlet",
-          "TIMELINE": "Timeline",
-          "VECTOR_MAP": "Vector Map"
-        },
-        "table": {
-          "TABLE": "Table",
-          "TABLE_STATIC": "Table static",
-          "DATATABLE": "Datatable",
-          "FOOTABLE": "Footable"
-        },
-        "form": {
-          "FORM": "Form",
-          "FORM_ELEMENTS": "Form elements",
-          "FORM_VALIDATION": "Form validation",
-          "FORM_WIZARD": "Form wizard"
-        },
-        "CHART": "Chart",
-        "pages": {
-          "PAGES": "Pages",
-          "PROFILE": "Profile",
-          "POST": "Post",
-          "SEARCH": "Search",
-          "INVOICE": "Invoice",
-          "LOCK_SCREEN": "Lock screen",
-          "SIGNIN": "Signin",
-          "SIGNUP": "Signup",
-          "FORGOT_PASSWORD": "Forgot password",
-          "404": "404"
+    "aside": {
+        "nav": {
+            "ps": {
+                "OVERVIEW": "Overview",
+                "PRICE": "Price List",
+                "ENABLE_SERVICE": "Activate",
+                "SERVICE": "Professional Service",
+                "LOG_STORE": "Log Store",
+                "API_CALL": "Rest API Invocation",
+                "CALLBACK": "Callback",
+                "CALLBACK_ADD": "Add Callback",
+                "CALLBACK_DEL": "Delete Callback",
+                "CALLBACK_SET": "Modify Callback",
+                "CALLBACK_GET": "Query Callback",
+                "VIP_CLUSTER": "VIP Cluster",
+                "MSG_SEND": "Send Message",
+                "OFFLINE_MSG": "Offline Message Store",
+                "LOG_DOWNLOAD": "Log Download",
+                "BROADCAST": "Broadcast",
+                "GROUP_MSG": "Group Message",
+                "SENSITIVE": "Sensitive Words",
+                "SENSITIVE_OPEN": "Activate Sensitive Words",
+                "SENSITIVE_UPLOAD": "Upload Sensitive Words",
+                "SENSITIVE_DOWNLOAD": "Download Sensitive Words",
+                "SENSITIVE_PARSE": "Test Sensitive Words",
+                "ANTI_SPAM": "Anti Spam",
+                "PROXY": "Oversea Proxy"
+            }
         }
-      },
-      "your_stuff": {
-        "YOUR_STUFF": "Your Stuff",
-        "PROFILE": "Profile",
-        "DOCUMENTS": "Documents"
-      }
     },
-    "MILESTONE": "Milestone",
-    "RELEASE": "Release"
-  },
-  "app": {
-    "common": {
-      "GRID": "Grid",
-      "OPERATIONS": "Operations",
-      "OPERATIONS_DEL": "Delete",
-      "OPERATIONS_EDIT": "Edit",
-      "OPERATIONS_EXP": "Export",
-      "OPERATIONS_APPLY": "Apply",
-      "OPERATIONS_SEARCH": "Search"
+    "app": {
+        "common": {
+            "GRID": "Grid",
+            "OPERATIONS": "Operations",
+            "OPERATIONS_DEL": "Delete",
+            "OPERATIONS_EDIT": "Edit",
+            "OPERATIONS_EXP": "Export",
+            "OPERATIONS_APPLY": "Apply",
+            "OPERATIONS_SEARCH": "Search",
+            "STEPS_1": "Step 1",
+            "STEPS_2": "Step 2",
+            "STEPS_3": "Step 3",
+            "NEXT": "Next",
+            "PREVIOUS": "Previous",
+            "DONE": "Done",
+            "EXIT": "Exit"
+        }
     },
-
-    "func01": {
-      "data": {
-        "COL_PROJECT": "Project",
-        "COL_TASK": "Task",
-        "COL_DATE": "Date"
-      }
+    "api": {
+        "common": {
+            "STEP1": "Setup",
+            "STEP2": "Execution",
+            "STEP3": "Response",
+            "RESPONSE": "Response",
+            "SERVER": "Server"
+        },
+        "callback_get": {
+            "org": "Org",
+            "app": "App"
+        },
+        "callback_add": {
+            "org": "Org",
+            "app": "App",
+            "name": "Name",
+            "msgTypes": "Message Type",
+            "hxSecret": "Easemob Secret",
+            "secret": "User Secret",
+            "targetUrl": "Callback Url",
+            "status": "Status",
+            "notifyUrl": "Notification Url"
+        },
+        "callback_set": {
+            "org": "Org",
+            "app": "App",
+            "name": "Callback Name",
+            "msgTypes": "Message Type",
+            "hxSecret": "Easemob Secret",
+            "secret": "User Secret",
+            "targetUrl": "Callback Url",
+            "status": "Status",
+            "notifyUrl": "Notification Url"
+        },
+        "callback_del": {
+            "org": "Org",
+            "app": "App",
+            "name": "Callback Name"
+        },
+        "enable_service": {
+            "appkey": "App Key",
+            "productType": "Product Type",
+            "startTime": "Start Time",
+            "dateType": "Date Type",
+            "openTime": "Date",
+            "status": "Status",
+            "operator": "Operator"
+        },
+        "logstore_set": {
+            "org": "Org",
+            "app": "App",
+            "chatfile_ttl": "Chat File TTL(s)",
+            "chatmessage_ttl": "Chat Message TTL(ms)"
+        },
+        "sensitive_open": {
+            "appKey": "App Key"
+        },
+        "sensitive_upload": {
+            "appKey": "App Key",
+            "wordList": "Sensitive Words"
+        },
+        "sensitive_download": {
+            "org": "Org",
+            "app": "App"
+        },
+        "sensitive_parse": {
+            "org": "Org",
+            "app": "App",
+            "content": "Test Content"
+        }
     }
-  }
 }
--- src/l10n/zh-cn.js
+++ src/l10n/zh-cn.js
@@ -1,180 +1,121 @@
 {
-  "aside": {
-    "nav": {
-      "ps": {
-        "OVERVIEW": "概览",
-        "PRICE": "价格目录",
-        "ENABLE_SERVICE": "开通服务",
-        "SERVICE": "增值服务",
-        "LOG_STORE": "聊天记录存储时间",
-        "API_CALL": "REST API调用次数",
-        "CALLBACK": "消息回调",
-        "CALLBACK_ADD": "新增消息回调",
-        "CALLBACK_DEL": "删除消息回调",
-        "CALLBACK_SET": "修改消息回调",
-        "CALLBACK_GET": "查询消息回调",
-        "VIP_CLUSTER": "VIP集群",
-        "MSG_SEND": "消息发送",
-        "OFFLINE_MSG": "离线消息存储",
-        "LOG_DOWNLOAD": "聊天记录下载",
-        "BROADCAST": "实时广播消息",
-        "GROUP_MSG": "群组消息",
-        "SENSITIVE": "敏感词过滤",
-        "SENSITIVE_OPEN": "开通敏感词过滤",
-        "SENSITIVE_UPLOAD": "上传敏感词",
-        "SENSITIVE_DOWNLOAD": "下载敏感词",
-        "SENSITIVE_PARSE": "测试敏感词",
-        "ANTI_SPAM": "反垃圾服务",
-        "PROXY": "海外代理"
-      },
-      "HEADER": "导航",
-      "DASHBOARD": "仪表盘",
-      "CALENDAR": "日历",
-      "EMAIL": "邮件",
-      "WIDGETS": "Widgets",
-      "components": {
-        "COMPONENTS": "Components",
-        "ui_kits": {
-          "UI_KITS": "UI Kits",
-          "BUTTONS": "Buttons",
-          "ICONS": "Icons",
-          "GRID": "Grid",
-          "BOOTSTRAP": "Bootstrap",
-          "SORTABLE": "Sortable",
-          "PORTLET": "Portlet",
-          "TIMELINE": "Timeline",
-          "VECTOR_MAP": "Vector Map"
-        },
-        "table": {
-          "TABLE": "Table",
-          "TABLE_STATIC": "Table static",
-          "DATATABLE": "Datatable",
-          "FOOTABLE": "Footable"
-        },
-        "form": {
-          "FORM": "Form",
-          "FORM_ELEMENTS": "Form elements",
-          "FORM_VALIDATION": "Form validation",
-          "FORM_WIZARD": "Form wizard"
-        },
-        "CHART": "Chart",
-        "pages": {
-          "PAGES": "Pages",
-          "PROFILE": "Profile",
-          "POST": "Post",
-          "SEARCH": "Search",
-          "INVOICE": "Invoice",
-          "LOCK_SCREEN": "Lock screen",
-          "SIGNIN": "Signin",
-          "SIGNUP": "Signup",
-          "FORGOT_PASSWORD": "Forgot password",
-          "404": "404"
+    "aside": {
+        "nav": {
+            "ps": {
+                "OVERVIEW": "概览",
+                "PRICE": "价格目录",
+                "ENABLE_SERVICE": "开通服务",
+                "SERVICE": "增值服务",
+                "LOG_STORE": "聊天记录存储时间",
+                "API_CALL": "REST API调用次数",
+                "CALLBACK": "消息回调",
+                "CALLBACK_ADD": "新增消息回调",
+                "CALLBACK_DEL": "删除消息回调",
+                "CALLBACK_SET": "修改消息回调",
+                "CALLBACK_GET": "查询消息回调",
+                "VIP_CLUSTER": "VIP集群",
+                "MSG_SEND": "消息发送",
+                "OFFLINE_MSG": "离线消息存储",
+                "LOG_DOWNLOAD": "聊天记录下载",
+                "BROADCAST": "实时广播消息",
+                "GROUP_MSG": "群组消息",
+                "SENSITIVE": "敏感词过滤",
+                "SENSITIVE_OPEN": "开通敏感词过滤",
+                "SENSITIVE_UPLOAD": "上传敏感词",
+                "SENSITIVE_DOWNLOAD": "下载敏感词",
+                "SENSITIVE_PARSE": "测试敏感词",
+                "ANTI_SPAM": "反垃圾服务",
+                "PROXY": "海外代理"
+            }
         }
-      },
-      "your_stuff": {
-        "YOUR_STUFF": "Your Stuff",
-        "PROFILE": "Profile",
-        "DOCUMENTS": "Documents"
-      }
     },
-    "MILESTONE": "Milestone",
-    "RELEASE": "Release"
-  },
-  "app": {
-    "common": {
-      "GRID": "查询结果",
-      "OPERATIONS": "操作",
-      "OPERATIONS_DEL": "删除",
-      "OPERATIONS_EDIT": "编辑",
-      "OPERATIONS_EXP": "导出",
-      "OPERATIONS_APPLY": "应用",
-      "OPERATIONS_SEARCH": "查询",
-      "STEPS_1": "步骤 1",
-      "STEPS_2": "步骤 2",
-      "STEPS_3": "步骤 3",
-      "NEXT": "下一步",
-      "PREVIOUS": "上一步",
-      "DONE": "完成",
-      "EXIT": "退出"
+    "app": {
+        "common": {
+            "GRID": "查询结果",
+            "OPERATIONS": "操作",
+            "OPERATIONS_DEL": "删除",
+            "OPERATIONS_EDIT": "编辑",
+            "OPERATIONS_EXP": "导出",
+            "OPERATIONS_APPLY": "应用",
+            "OPERATIONS_SEARCH": "查询",
+            "STEPS_1": "步骤 1",
+            "STEPS_2": "步骤 2",
+            "STEPS_3": "步骤 3",
+            "NEXT": "下一步",
+            "PREVIOUS": "上一步",
+            "DONE": "完成",
+            "EXIT": "退出"
+        }
     },
-    "logstore": {
-      "STEP1_HEADER": "查询用户",
-      "STEP1_DESC": "请先查找想要操作的用户",
-      "STEP2_HEADER": "设置参数",
-      "STEP2_DESC": "这里您可以设置消息存储的天数",
-      "STEP3_HEADER": "确认",
-      "STEP3_DESC": "确认无误后开通该服务"
+    "api": {
+        "common": {
+            "STEP1": "设置参数",
+            "STEP2": "确认执行",
+            "STEP3": "查看结果",
+            "RESPONSE": "请求响应",
+            "SERVER": "服务器地址"
+        },
+        "callback_get": {
+            "org": "组织机构标识",
+            "app": "应用标识"
+        },
+        "callback_add": {
+            "org": "组织机构标识",
+            "app": "应用标识",
+            "name": "回调名称",
+            "msgTypes": "消息类型",
+            "hxSecret": "环信秘钥",
+            "secret": "用户秘钥",
+            "targetUrl": "回调地址",
+            "status": "状态",
+            "notifyUrl": "通知地址"
+        },
+        "callback_set": {
+            "org": "组织机构标识",
+            "app": "应用标识",
+            "name": "回调名称",
+            "msgTypes": "消息类型",
+            "hxSecret": "环信秘钥",
+            "secret": "用户秘钥",
+            "targetUrl": "回调地址",
+            "status": "状态",
+            "notifyUrl": "通知地址"
+        },
+        "callback_del": {
+            "org": "组织机构标识",
+            "app": "应用标识",
+            "name": "回调名称"
+        },
+        "enable_service": {
+            "appkey": "App Key",
+            "productType": "服务类型",
+            "startTime": "开始时间",
+            "dateType": "时长类型",
+            "openTime": "时长",
+            "status": "状态",
+            "operator": "操作者"
+        },
+        "logstore_set": {
+            "org": "组织机构标识",
+            "app": "应用标识",
+            "chatfile_ttl": "聊天文件过期时间(秒)",
+            "chatmessage_ttl": "聊天消息过期时间(毫秒)"
+        },
+        "sensitive_open": {
+            "appKey": "App Key"
+        },
+        "sensitive_upload": {
+            "appKey": "App Key",
+            "wordList": "敏感词"
+        },
+        "sensitive_download": {
+            "org": "组织机构标识",
+            "app": "应用标识"
+        },
+        "sensitive_parse": {
+            "org": "组织机构标识",
+            "app": "应用标识",
+            "content": "测试内容"
+        }
     }
-  },
-  "api": {
-    "common": {
-      "STEP1": "设置参数",
-      "STEP2": "确认执行",
-      "STEP3": "查看结果",
-      "RESPONSE": "请求响应",
-      "SERVER": "服务器地址"
-    },
-    "callback_get": {
-      "org": "组织机构标识",
-      "app": "应用标识"
-    },
-    "callback_add": {
-      "org": "组织机构标识",
-      "app": "应用标识",
-      "name": "回调名称",
-      "msgTypes": "消息类型",
-      "hxSecret": "环信秘钥",
-      "secret": "用户秘钥",
-      "targetUrl": "回调地址",
-      "status": "状态",
-      "notifyUrl": "通知地址"
-    },
-    "callback_set": {
-      "org": "组织机构标识",
-      "app": "应用标识",
-      "name": "回调名称",
-      "msgTypes": "消息类型",
-      "hxSecret": "环信秘钥",
-      "secret": "用户秘钥",
-      "targetUrl": "回调地址",
-      "status": "状态",
-      "notifyUrl": "通知地址"
-    },
-    "callback_del": {
-      "org": "组织机构标识",
-      "app": "应用标识",
-      "name": "回调名称"
-    },
-    "enable_service": {
-      "appkey": "App Key",
-      "productType": "服务类型",
-      "startTime": "开始时间",
-      "dateType": "时长类型",
-      "openTime": "时长",
-      "status": "状态",
-      "operator": "操作者"
-    },
-    "logstore_set": {
-      "org": "组织机构标识",
-      "app": "应用标识",
-      "chatfile_ttl": "聊天文件过期时间(秒)",
-      "chatmessage_ttl": "聊天消息过期时间(毫秒)"
-    },
-    "sensitive_open": {
-      "appKey": "App Key"
-    },
-    "sensitive_upload": {
-      "appKey": "App Key",
-      "wordList": "敏感词"
-    },
-    "sensitive_download": {
-      "org": "组织机构标识",
-      "app": "应用标识"
-    },
-    "sensitive_parse": {
-      "org": "组织机构标识",
-      "app": "应用标识",
-      "content": "测试内容"
-    }
-  }
 }
