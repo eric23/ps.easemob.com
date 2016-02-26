678a0727bed95ef840209869c8562499a1e5491c
eric23 <yanjun23@sina.com>
2016-02-26 21:07:21 +0800

    Initialize get token pages.

--- src/js/controllers/ps/query/token.js
+++ src/js/controllers/ps/query/token.js
@@ -1,3 +1,17 @@
 /**
  * Created by eric on 16/2/26.
  */
+app.controller('TokenController', ['$scope', '$http', 'toaster', 'apiService', function($scope, $http, toaster, apiService) {
+    $scope.config = {};
+
+    if(!apiService.initialized()) {
+        apiService.init();
+        setTimeout(function(){
+            $scope.config.servers = apiService.getServerNames();
+        }, 100);
+    } else {
+        $scope.config.servers = apiService.getServerNames();
+    }
+
+
+}]);
